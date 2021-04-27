0cd8cebe55fc4617cdcd775fd6ca47aa1603b628
Joshua Trujillo <Jtriley15@gmail.com>
2021-04-26 20:26:49 -0500

    Update ERC1620.sol

--- client/contracts/ERC1620.sol
+++ client/contracts/ERC1620.sol
@@ -3,19 +3,23 @@ pragma solidity ^0.8.4;
 
 import "./interfaces/IERC1620.sol";
 
-contract ERC1620 is IERC1620 {
-
-    struct StreamIndex {
-        Stream stream;
-        uint256 streamId;
-    }
+abstract contract ERC1620 is IERC1620 {
 
     mapping(address => uint256) balances;
     Stream[] streamDatabase;
     address handlerToken;
 
     modifier streamActive(uint256 _streamId) {
-        require(streamDatabase.length > _streamId, "Error: Inactive Stream");
+        require(streamDatabase.length > _streamId, "inactive stream");
+        _;
+    }
+
+    modifier validParticipant(uint256 _streamId, address _addr) {
+        require(
+            streamDatabase[_streamId].recipient == _addr ||
+            streamDatabase[_streamId].sender == _addr,
+            "address is not participating in this stream"
+        );
         _;
     }
 
@@ -26,10 +30,17 @@ contract ERC1620 is IERC1620 {
         address _addr
     ) 
         public
-        view
+        streamActive(_streamId)
+        validParticipant(_streamId, _addr)
         returns (uint256)
     {
-        
+        snapshot(_streamId);
+        Stream memory stream = streamDatabase[_streamId];
+        if (stream.sender == _addr) {
+            return stream.payment - stream.balance;
+        } else {
+            return stream.balance;
+        }
     }
 
     function getStream(
@@ -39,8 +50,7 @@ contract ERC1620 is IERC1620 {
         view
         override
         streamActive(_streamId)
-        returns 
-    (
+        returns (
         address sender,
         address recipient,
         address tokenAddress,
@@ -51,16 +61,15 @@ contract ERC1620 is IERC1620 {
         uint256 interval
     )
     {
-        Stream memory queriedStream = streamDatabase[_streamId];
         return (
-            queriedStream.sender,
-            queriedStream.recipient,
-            queriedStream.tokenAddress,
-            queriedStream.balance,
-            queriedStream.start,
-            queriedStream.stop,
-            queriedStream.payment,
-            queriedStream.interval
+            streamDatabase[_streamId].sender,
+            streamDatabase[_streamId].recipient,
+            streamDatabase[_streamId].tokenAddress,
+            streamDatabase[_streamId].balance,
+            streamDatabase[_streamId].start,
+            streamDatabase[_streamId].stop,
+            streamDatabase[_streamId].payment,
+            streamDatabase[_streamId].interval
         );
     }
 
@@ -78,14 +87,19 @@ contract ERC1620 is IERC1620 {
         require(_tokenAddress == handlerToken, "incompatible token");
         require(_recipient != address(0x00), "recipient is a zero address");
         require(_recipient != address(this), "recipient is this contract");
-        require(_recipient != msg.sender, "recpient is the sender");
+        require(_recipient != msg.sender, "recipient is the sender");
         require(_payment > 0, "payment cannot be zero");
-        require(_startBlock >= block.number, "startBlock must be a future block");
-        require(_stopBlock >= _startBlock, "stopBlock must be after startBlock");
+        require(
+            _startBlock >= block.number,
+            "startBlock must be a future block"
+        );
+        require(_stopBlock > _startBlock, "stopBlock must be after startBlock");
         require(_interval > 0, "interval must be greater than 0");
+        require(
+            (_stopBlock - _startBlock) % _interval == 0, 
+            "interval must be divisible by the number of blocks");
 
         Stream memory stream;
-        StreamIndex memory streamIndex;
 
         stream.sender = msg.sender;
         stream.recipient = _recipient;
@@ -95,9 +109,6 @@ contract ERC1620 is IERC1620 {
         stream.stop = _stopBlock;
         stream.payment = _payment;
         stream.interval = _interval;
-
-        streamIndex.stream = stream;
-        streamIndex.streamId = streamDatabase.length;
 
         emit LogCreate(
             streamDatabase.length,
@@ -110,6 +121,8 @@ contract ERC1620 is IERC1620 {
             _interval
         );
 
+        streamDatabase.push(stream);
+
     }
 
     function withdraw(
@@ -120,10 +133,24 @@ contract ERC1620 is IERC1620 {
         override
         streamActive(_streamId)
     {
-
+        require(
+            msg.sender == streamDatabase[_streamId].recipient, 
+            "must be recipient"
+        );
+        // Snapshot issues ?
     }
 
-    function preciseDivision(uint256 _a, uint256 _b, uint256 _precision) public view returns (uint256) {
-        return _a * (10 ** _precision) / _b;
+    function snapshot(uint256 _streamId) private {
+
+        Stream memory stream = streamDatabase[_streamId];
+        if (block.number <= stream.start || block.number >= stream.stop) {
+            return;
+        }
+        uint256 totalBlocks = stream.stop - stream.start;
+        uint256 intervalValue = stream.payment / (totalBlocks / stream.interval);
+        uint256 intervalsPassed = (block.number - stream.start) / stream.interval;
+
+        streamDatabase[_streamId].balance = intervalValue * intervalsPassed;
     }
+
 }
